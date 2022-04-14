cc7b28f10ad359447bc164b317b390ecaf0b3996
Anastasia Drogunova <adrogunova@gmail.com>
2022-04-14 11:47:39 +0300

    убрала экшн бар

--- app/src/main/java/com/nestdev/trueconftest1/MainFragment.kt
+++ app/src/main/java/com/nestdev/trueconftest1/MainFragment.kt
@@ -1,23 +1,23 @@
 package com.nestdev.trueconftest1
 
+import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
-import android.app.ActionBar
+import android.content.Context
+import android.graphics.Point
+import android.os.Build
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.MotionEvent
-import android.view.View
-import android.view.ViewGroup
+import android.util.DisplayMetrics
+import android.view.*
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
-import android.widget.FrameLayout
+import android.view.animation.TranslateAnimation
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.core.animation.addListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.whenResumed
-import com.nestdev.trueconftest1.R
 import com.nestdev.trueconftest1.databinding.FragmentAnimatedTextviewBinding
+
 
 /**
  * Решила создать отдельный фрагмент для анимации
@@ -32,6 +32,10 @@ class MainFragment : Fragment() {
     private val binding get() = _binding!!
 //    private val textMovingAnim: Animation =
 //        AnimationUtils.loadAnimation(requireContext(), R.anim.up_down_anim)
+    //private lateinit var topToBottomAnimation : TranslateAnimation
+    private lateinit var toBottomAnimator: ObjectAnimator
+    private lateinit var toTopAnimator: ObjectAnimator
+    private lateinit var displayMetrics : DisplayMetrics
 
     private val coordsArray = Array<Int>(2) { 0 }
     private var layoutPlaceParams = LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
@@ -46,10 +50,49 @@ class MainFragment : Fragment() {
      }
 
 
+    @RequiresApi(Build.VERSION_CODES.R)
     @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val wm = view.context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+//        val metric = wManager.currentWindowMetrics
+//        val windowInsets = metric.windowInsets
+//        val insets = windowInsets.getInsetsIgnoringVisibility(WindowInsets.Type.navigationBars())
+//        insets.
+
+//
+//
+        val deviceHeight = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            wm.currentWindowMetrics.bounds.height()
+        } else {
+            val point = Point()
+            wm.defaultDisplay.getSize(point)
+            point.x
+        }
+
+
+//        toBottomAnimator = AnimatorInflater.loadAnimator(requireContext(), R.animator.to_bottom_animator) as ObjectAnimator
+//        toTopAnimator = AnimatorInflater.loadAnimator(requireContext(), R.animator.to_top_animator) as ObjectAnimator
+//        toBottomAnimator.propertyName()
+        //val screenHeight =
         with(binding) {
+            val height = deviceHeight.toFloat()
+            toBottomAnimator = ObjectAnimator.ofFloat(animatedTextView,"translationY", 0f, height - 400f)
+            toTopAnimator = ObjectAnimator.ofFloat(animatedTextView,"translationY",coordsArray[1].toFloat(), 0f)
+            println("HERE ${view.height.toFloat()}")
+               toBottomAnimator.duration = 70000
+                toBottomAnimator.startDelay = 1000
+                toTopAnimator.duration = 7000
+
+            toBottomAnimator.addListener {
+                onEnd -> toTopAnimator.start()
+            }
+            toTopAnimator.addListener {
+                onEnd -> toBottomAnimator.start()
+            }
+//            val topToBottomAnimation = TranslateAnimation(TranslateAnimation.RELATIVE_TO_SELF, 0f, TranslateAnimation.RELATIVE_TO_SELF, 0f, TranslateAnimation.RELATIVE_TO_SELF, 0f, TranslateAnimation.RELATIVE_TO_SELF, height - )
+//           topToBottomAnimation.duration = 5000
             animatedTextView.setOnClickListener {
                 //TODO
             }
@@ -61,6 +104,13 @@ class MainFragment : Fragment() {
                     layoutPlaceParams.topMargin = coordsArray[1]
                     layoutPlaceParams.leftMargin = coordsArray[0]
                     animatedTextView.layoutParams = layoutPlaceParams
+
+                    toBottomAnimator = ObjectAnimator.ofFloat(animatedTextView,"translationY", 0f, height - coordsArray[1].toFloat() - 100)
+                    toTopAnimator = ObjectAnimator.ofFloat(animatedTextView,"translationY",coordsArray[1].toFloat(), 0f)
+                    toBottomAnimator.start()
+            //        val topToBottomAnimation = TranslateAnimation(TranslateAnimation.RELATIVE_TO_SELF, 0f, TranslateAnimation.RELATIVE_TO_SELF, 0f, TranslateAnimation.RELATIVE_TO_PARENT, coordsArray[1].toFloat(), TranslateAnimation.RELATIVE_TO_SELF, height -  coordsArray[1] - 30)
+//                    topToBottomAnimation.duration = 5000
+//                    animatedTextView.startAnimation(topToBottomAnimation)
                 }
                 return@OnTouchListener true
             })
