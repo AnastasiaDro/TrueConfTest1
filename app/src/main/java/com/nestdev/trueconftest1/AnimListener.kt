5a80482f10c6d351952d4478a1f3838bdd375338
Anastasia Drogunova <adrogunova@gmail.com>
2022-04-15 11:31:54 +0300

    DONE:  - вынесла AnimListener в отдельный класс  TODO:  - ОТРЕФАКТОРИТЬ

---
+++ app/src/main/java/com/nestdev/trueconftest1/AnimListener.kt
@@ -0,0 +1,24 @@
+package com.nestdev.trueconftest1
+
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
+
+class AnimListener(private val nextAnimator: ObjectAnimator) : AnimatorListenerAdapter()
+{
+    override fun onAnimationEnd(animation: Animator?, isReverse: Boolean) {
+        super.onAnimationEnd(animation, isReverse)
+        when  {
+            animationCounter % 2 != 0 -> nextAnimator.setFloatValues(0f - marginTop, newFloatValue)
+            animationCounter % 2 == 0  -> nextAnimator.setFloatValues(newFloatValue, 0f - marginTop)
+        }
+        nextAnimator.start()
+        animationCounter++
+    }
+
+    companion object {
+        var marginTop = 0f
+        var newFloatValue = 0f
+        var animationCounter = 0
+    }
+}
--- app/src/main/java/com/nestdev/trueconftest1/MainFragment.kt
+++ app/src/main/java/com/nestdev/trueconftest1/MainFragment.kt
@@ -1,7 +1,5 @@
 package com.nestdev.trueconftest1
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.content.Context
@@ -12,11 +10,7 @@ import android.util.DisplayMetrics
 import android.util.TypedValue
 import android.view.*
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
-import android.widget.FrameLayout
 import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -30,6 +24,11 @@ import com.nestdev.trueconftest1.databinding.FragmentAnimatedTextviewBinding
  *
  * @author Anastasia Drogunova
  */
+
+const val ANIMATION_DELAY = 5000L
+const val ANIMATION_DURATION = 3500L
+
+
 class MainFragment : Fragment() {
     private val viewModel by viewModels<MainFragmentViewModel>()
     private var _binding: FragmentAnimatedTextviewBinding? = null
@@ -49,19 +48,17 @@ class MainFragment : Fragment() {
         return binding.root
     }
 
-
     @RequiresApi(Build.VERSION_CODES.R)
     @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val wm = view.context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val windowManager = view.context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
         val deviceHeight = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            wm.currentWindowMetrics.bounds.height()
+            windowManager.currentWindowMetrics.bounds.height()
         } else {
             val point = Point()
-            wm.defaultDisplay.getSize(point)
-            point.x
+            windowManager.defaultDisplay.getSize(point)
+            point.y
         }
 
         with(binding) {
@@ -71,33 +68,29 @@ class MainFragment : Fragment() {
             toTopAnimator = ObjectAnimator.ofFloat(
                 animatedTextView,
                 "translationY",
-                coordsArray[1].toFloat(),
+                0f,
                 0f
             )
-
-            toTopAnimator.duration = 700
-
 
 
             toBottomAnimator.addListener(AnimListener(toTopAnimator))
             toTopAnimator.addListener(AnimListener(toBottomAnimator))
-            toBottomAnimator.duration = 7000
-            toTopAnimator.duration = 7000
-            toBottomAnimator.startDelay = 1000
+            toBottomAnimator.duration = ANIMATION_DURATION
+            toTopAnimator.duration = ANIMATION_DURATION
+            toBottomAnimator.startDelay = ANIMATION_DELAY
+
             animatedTextView.setOnClickListener {
                 toBottomAnimator.pause()
                 toTopAnimator.pause()
-
             }
+
             mainFragmentFrame.setOnTouchListener(View.OnTouchListener { view, motionEvent ->
                 animatedTextView.setTextColor(requireContext().getColor(R.color.clicked_text_color))
                 if (motionEvent.action == MotionEvent.ACTION_UP) {
                     AnimListener.animationCounter = 0
-                    coordsArray[0] = motionEvent.x.toInt()
-                    coordsArray[1] = motionEvent.y.toInt()
                     view.performClick()
-                    layoutPlaceParams.topMargin = coordsArray[1]
-                    layoutPlaceParams.leftMargin = coordsArray[0]
+                    layoutPlaceParams.topMargin = motionEvent.y.toInt()
+                    layoutPlaceParams.leftMargin = motionEvent.x.toInt()
                     animatedTextView.layoutParams = layoutPlaceParams
                     val pixels = convertSpToPixels(animatedTextView.textSize, requireContext())
                     AnimListener.newFloatValue = height - motionEvent.y - pixels*2
@@ -109,22 +102,8 @@ class MainFragment : Fragment() {
                 }
                 return@OnTouchListener true
             })
-
         }
     }
-
-    private fun performAnimation() {
-
-    }
-
-    private fun moveToPoint(textView: TextView) {
-
-    }
-
-    companion object {
-        fun create() = MainFragment()
-    }
-
 
     fun convertSpToPixels(sp: Float, context: Context): Int {
         return TypedValue.applyDimension(
@@ -134,25 +113,8 @@ class MainFragment : Fragment() {
         )
             .toInt()
     }
-}
-
-class AnimListener(private val nextAnimator: ObjectAnimator) : AnimatorListenerAdapter()
-{
-
-    override fun onAnimationEnd(animation: Animator?, isReverse: Boolean) {
-        super.onAnimationEnd(animation, isReverse)
-        when  {
-            animationCounter % 2 != 0 -> nextAnimator.setFloatValues(0f - marginTop, newFloatValue)
-            animationCounter % 2 == 0  -> nextAnimator.setFloatValues(newFloatValue, 0f - marginTop)
-        }
-        nextAnimator.start()
-        animationCounter++
-    }
 
     companion object {
-        var marginTop = 0f
-        var newFloatValue = 0f
-        var animationCounter = 0
+        fun create() = MainFragment()
     }
 }
-
